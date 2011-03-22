896d647932c953a301b63e6d012bd26bee0759a1
PichuChen <pichubaby@gmail.com>
2011-03-23 01:04:41 +0800

    move sendStatusCode to lib/lib_common.php

--- lib/lib_common.php
+++ lib/lib_common.php
@@ -243,7 +243,7 @@ function adminAuthenticate($mode){
 			break;
 	}
 }
-
+ 
 /* 取得 (Transparent) Proxy 提供之 IP 參數 */
 function getREMOTE_ADDR(){
 	// 同時有 VIA 和 FORWARDED_FOR 較可能為 Proxy
@@ -253,5 +253,57 @@ function getREMOTE_ADDR(){
 		return ($tmp[0] != 'unknown' ? $tmp[0] : $_SERVER['REMOTE_ADDR']);
 	}
 	return $_SERVER['REMOTE_ADDR'];
+}
+
+
+/*送出狀態碼*/
+function sendStatusCode($code = "200"){
+	$codesList = array(
+		    100 => 'Continue',
+		    101 => 'Switching Protocols',
+		    200 => 'OK',
+		    201 => 'Created',
+		    202 => 'Accepted',
+		    203 => 'Non-Authoritative Information',
+		    204 => 'No Content',
+		    205 => 'Reset Content',
+		    206 => 'Partial Content',
+		    300 => 'Multiple Choices',
+		    301 => 'Moved Permanently',
+		    302 => 'Found',
+		    303 => 'See Other',
+		    304 => 'Not Modified',
+		    305 => 'Use Proxy',
+		    306 => '(Unused)',
+		    307 => 'Temporary Redirect',
+		    400 => 'Bad Request',
+		    401 => 'Unauthorized',
+		    402 => 'Payment Required',
+		    403 => 'Forbidden',
+		    404 => 'Not Found',
+		    405 => 'Method Not Allowed',
+		    406 => 'Not Acceptable',
+		    407 => 'Proxy Authentication Required',
+		    408 => 'Request Timeout',
+		    409 => 'Conflict',
+		    410 => 'Gone',
+		    411 => 'Length Required',
+		    412 => 'Precondition Failed',
+		    413 => 'Request Entity Too Large',
+		    414 => 'Request-URI Too Long',
+		    415 => 'Unsupported Media Type',
+		    416 => 'Requested Range Not Satisfiable',
+		    417 => 'Expectation Failed',
+		    500 => 'Internal Server Error',
+		    501 => 'Not Implemented',
+		    502 => 'Bad Gateway',
+		    503 => 'Service Unavailable',
+		    504 => 'Gateway Timeout',
+		    505 => 'HTTP Version Not Supported'
+		);
+
+
+	header("HTTP/1.0 {$code} {$codeList[$code]}");
+
 }
 ?>
