b004a16a9ddd3fabc6bd58ffbb8ee05621911837
PichuChen <Pichubaby@gmail.com>
2011-03-23 00:24:50 +0800

    Get data From PIO

--- lib/lib_show.php
+++ lib/lib_show.php
@@ -2,7 +2,10 @@
 function actSHOW(){ 
 	require("./lib/lib_pio.php");
 	print_r($PIO->fetchThreadList());
-	
+	$SelectArgv = 'no,sub,name,now,category,tim';
+	//$SelectArgv = "no,sub";
+   	print_r($PIO->fetchPosts($PIO->fetchThreadList(),$SelectArgv));
+		
 //	require("")
 
 
